d487d2e3dd3d42580420838af36950db27c6cf9f
Vitalii <vitaliyzaichkin@gmail.com>
2018-09-25 16:41:23 +0300

    'init2'

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function() {
     return view('comments/home');
 });
 
+Route::get('/', 'CommentController@index')->name('home');
+
 Route::resource('comment', 'CommentController');
